70820a878c3abbcd8dd94caebff038c4b97edc15
Zaph-x <35431127+Zaph-x@users.noreply.github.com>
2025-02-16 14:40:11 +0100

    Moved trade handler to handlers folder
    This commit moves the trade_handler to the handlers folder and adds a
    new base handler to the folder. The base handler should be used as a
    base class

---
+++ src/handlers/trade_handler.py
@@ -0,0 +1,10 @@
+from requests import Session
+
+
+class TradeHandler:
+    def __init__(self, session: Session):
+        if "Authorization" not in session.headers:
+            raise ValueError("No authorization token found in session")
+        self.session = session
+
+    def place_order
