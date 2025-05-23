7e977b10ef7cf395444fca881c79668e5bd2ec19
Zaph-x <35431127+Zaph-x@users.noreply.github.com>
2025-05-23 22:31:05 +0200

    Added to_json method

--- src/data_models/balance_information.py
+++ src/data_models/balance_information.py
@@ -14,6 +14,22 @@ class BalanceInformation:
         self.net_positions_count = data.get("NetPositionsCount", 0)
         self.orders_count = data.get("OrdersCount", 0)
 
+    def to_json(self) -> dict:
+        """
+        Convert the balance information to JSON format.
+
+        Returns:
+            dict: The balance information in JSON format.
+        """
+        return {
+            "CashBalance": self.cash_balance,
+            "Currency": self.Currency,
+            "TotalValue": self.total_value,
+            "MarginAvailableForTrading": self.margin_available,
+            "NetPositionsCount": self.net_positions_count,
+            "OrdersCount": self.orders_count,
+        }
+
     def get_property(self, key: str):
         """
         Get the value of a specific property from the balance information.
